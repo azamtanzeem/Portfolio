8f5a9f073dab5a216ae1da369fb80ea97050eeb3
Azam Tanzeem <150652160+azamtanzeem@users.noreply.github.com>
2026-06-10 08:49:48 +0530

    Create 9891_customer_details.sql

---
+++ 01-sql/SQL/Day05_JOINs/SQL/Day05_JOINs/9891_customer_details.sql
@@ -0,0 +1,10 @@
+-- Problem: Customer Details
+-- Platform: StrataScratch #9891
+-- Difficulty: Easy
+-- JOIN Type: LEFT JOIN
+-- Date: 2026-06-10
+
+SELECT c.first_name, c.last_name, c.city, o.order_details
+FROM customers c 
+LEFT JOIN orders o ON c.customer_id = o.customer_id
+ORDER BY c.first_name, o.order_details
