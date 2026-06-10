1e2e61a11b5d66e140aa2507a69227f2e453590d
Azam Tanzeem <150652160+azamtanzeem@users.noreply.github.com>
2026-06-10 08:52:34 +0530

    Create 9913_order_details.sql

---
+++ 01-sql/Day05_JOINs/9913_order_details.sql
@@ -0,0 +1,11 @@
+-- Problem: Order Details
+-- Platform: StrataScratch #9913
+-- Difficulty: Easy
+-- JOIN Type: LEFT JOIN (multi-table)
+-- Date: 2026-06-10
+
+SELECT o.order_date, o.order_details, o.total_order_cost, c.first_name
+FROM customers c 
+LEFT JOIN orders o ON c.id = o.cust_id
+WHERE c.first_name IN ('Jill', 'Eva')
+ORDER BY o.cust_id
