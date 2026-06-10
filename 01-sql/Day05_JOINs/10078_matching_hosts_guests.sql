e74c3de4267db5401c7dbc697e73de5ff5e8a5aa
Azam Tanzeem <150652160+azamtanzeem@users.noreply.github.com>
2026-06-10 08:55:09 +0530

    Create 10078_matching_hosts_guests.sql

---
+++ 01-sql/Day05_JOINs/10078_matching_hosts_guests.sql
@@ -0,0 +1,11 @@
+-- Problem: Find Matching Hosts and Guests
+-- Platform: StrataScratch #10078
+-- Difficulty: Medium
+-- JOIN Type: INNER JOIN on multiple conditions
+-- Date: 2026-06-10
+
+SELECT DISTINCT h.host_id, g.guest_id
+FROM airbnb_hosts h 
+INNER JOIN airbnb_guests g 
+    ON h.nationality = g.nationality
+    AND h.gender = g.gender
