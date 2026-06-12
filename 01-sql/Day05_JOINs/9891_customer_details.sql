f5588719b54397ae72dd9694089caaef567d121c
Azam Tanzeem <150652160+azamtanzeem@users.noreply.github.com>
2026-06-12 06:51:54 +0530

    Update 9891_customer_details.sql

--- 01-sql/Day05_JOINs/9891_customer_details.sql
+++ 01-sql/Day05_JOINs/9891_customer_details.sql
@@ -1,8 +1,8 @@
--- Problem: Customer Details
--- Platform: StrataScratch #9891
--- Difficulty: Easy
--- JOIN Type: LEFT JOIN
--- Date: 2026-06-10
+Problem: Customer Details
+Platform: StrataScratch #9891
+Difficulty: Easy
+JOIN Type: LEFT JOIN
+Date: 2026-06-10
 
 SELECT c.first_name, c.last_name, c.city, o.order_details
 FROM customers c 
