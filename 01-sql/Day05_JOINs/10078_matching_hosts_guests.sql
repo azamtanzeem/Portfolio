62df43971608fbc389d0da8416d61caaaffe1b2c
Azam Tanzeem <150652160+azamtanzeem@users.noreply.github.com>
2026-06-12 06:50:07 +0530

    Update 10078_matching_hosts_guests.sql

--- 01-sql/Day05_JOINs/10078_matching_hosts_guests.sql
+++ 01-sql/Day05_JOINs/10078_matching_hosts_guests.sql
@@ -1,8 +1,8 @@
--- Problem: Find Matching Hosts and Guests
--- Platform: StrataScratch #10078
--- Difficulty: Medium
--- JOIN Type: INNER JOIN on multiple conditions
--- Date: 2026-06-10
+Problem: Find Matching Hosts and Guests
+Platform: StrataScratch #10078
+Difficulty: Medium
+JOIN Type: INNER JOIN on multiple conditions
+Date: 2026-06-10
 
 SELECT DISTINCT h.host_id, g.guest_id
 FROM airbnb_hosts h 
